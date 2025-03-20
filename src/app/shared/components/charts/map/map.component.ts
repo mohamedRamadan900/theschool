79d3151cc3cd5932f5d9fa75b0c4e7c7602e3a96
Mahmoud Meky <mahmoudmeky2017@gmail.com>
2025-03-20 22:48:54 +0200

    feat: update map component to accept dynamic height and improve marker icon handling

--- src/app/shared/components/charts/map/map.component.ts
+++ src/app/shared/components/charts/map/map.component.ts
@@ -1,22 +1,22 @@
-import { Component, Input, Output, EventEmitter, OnInit, AfterViewInit } from '@angular/core';
+import { Component, Input, Output, EventEmitter, OnInit, AfterViewInit, signal, input } from '@angular/core';
 import * as L from 'leaflet';
 import { CardComponent } from '../../layout/card/card.component';
 
 // Add this fix for marker icons //Customize Marker
-// const iconRetinaUrl = 'assets/marker-icon-2x.png';
-// const iconUrl = 'assets/marker-icon.png';
+const iconRetinaUrl = 'assets/map/marker.png';
+const iconUrl = 'assets/map/marker.png';
 // const shadowUrl = 'assets/marker-shadow.png';
-// const iconDefault = L.icon({
-//     iconRetinaUrl,
-//     iconUrl,
-//     shadowUrl,
-//     iconSize: [25, 41],
-//     iconAnchor: [12, 41],
-//     popupAnchor: [1, -34],
-//     tooltipAnchor: [16, -28],
-//     shadowSize: [41, 41]
-// });
-// L.Marker.prototype.options.icon = iconDefault;
+const iconDefault = L.icon({
+    iconRetinaUrl,
+    iconUrl,
+    // shadowUrl,
+    iconSize: [25, 25],
+    iconAnchor: [25, 25],
+    popupAnchor: [1, -34],
+    tooltipAnchor: [16, -28],
+    shadowSize: [41, 41]
+});
+L.Marker.prototype.options.icon = iconDefault;
 
 export interface MapMarker {
     lat: number;
@@ -32,10 +32,11 @@ export interface MapMarker {
     imports: [CardComponent]
 })
 export class MapComponent implements AfterViewInit {
-    @Input() title: string;
-    @Input() markers: MapMarker[] = [];
-    @Input() center: [number, number] = [51.505, -0.09]; // Default to London
-    @Input() zoom: number = 13;
+    title = input<string>('');
+    markers = input<MapMarker[]>([]);
+    center = input<[number, number]>([51.505, -0.09]); // Default to London
+    zoom = input<number>(13);
+    height = input<string>('400px');
     @Output() onMarkerClick = new EventEmitter<MapMarker>();
 
     private map!: L.Map;
@@ -46,14 +47,14 @@ export class MapComponent implements AfterViewInit {
     }
 
     private initMap(): void {
-        this.map = L.map('map').setView(this.center, this.zoom);
+        this.map = L.map('map').setView(this.center(), this.zoom());
         L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-            attribution: '© OpenStreetMap contributors'
+            // attribution: '© OpenStreetMap contributors'
         }).addTo(this.map);
     }
 
     private addMarkers(): void {
-        this.markers.forEach((marker) => {
+        this.markers().forEach((marker) => {
             const leafletMarker = L.marker([marker.lat, marker.lng])
                 .bindPopup(marker.popup || marker.title)
                 .addTo(this.map);
