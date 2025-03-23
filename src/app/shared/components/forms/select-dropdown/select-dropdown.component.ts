fc9e624fa1333d603e6d21820a03f972f759a246
Mahmoud Meky <mahmoudmeky2017@gmail.com>
2025-03-24 00:00:00 +0200

    Implement dynamic select dropdown with multi-select capability and filter options

--- src/app/shared/components/forms/select-dropdown/select-dropdown.component.ts
+++ src/app/shared/components/forms/select-dropdown/select-dropdown.component.ts
@@ -1,11 +1,63 @@
-import { Component } from '@angular/core';
+import { Component, input, Output, EventEmitter, signal } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { DropdownModule } from 'primeng/dropdown';
+import { MultiSelectChangeEvent, MultiSelectModule } from 'primeng/multiselect';
+
+interface Option {
+    value: any;
+    label: string;
+}
 
 @Component({
-  selector: 'app-select-dropdown',
-  imports: [],
-  templateUrl: './select-dropdown.component.html',
-  styleUrl: './select-dropdown.component.scss'
+    selector: 'app-select-dropdown',
+    standalone: true,
+    imports: [CommonModule, FormsModule, DropdownModule, MultiSelectModule],
+    templateUrl: './select-dropdown.component.html',
+    styleUrl: './select-dropdown.component.scss'
 })
 export class SelectDropdownComponent {
+    label = input<string>('Filter Options');
+    options = input<Option[]>([]);
+    allOption: Option = { value: 'All_Options_SelectDropdownComponent', label: 'All' };
+    optionsWithAll = signal<Option[]>([]);
+    placeholder = input<string>('Select options');
+    isMultiple = input<boolean>(false);
+    @Output() selectionChange = new EventEmitter<any[]>();
 
+    selectedValues = signal<any[]>([]);
+    private isAllOptionsChecked: boolean = false;
+
+    onSelectionChange(event: MultiSelectChangeEvent): void {
+        let updatedSelection: Option[] = event.value;
+
+        if (this.isMultiple()) {
+            updatedSelection = this.handleMultipleSelection(updatedSelection, event.itemValue);
+            this.selectedValues.set(updatedSelection);
+        } else {
+            this.selectedValues.set(updatedSelection);
+        }
+
+        const emitValue = this.isMultiple() ? this.selectedValues() : [this.selectedValues()];
+        this.selectionChange.emit(emitValue);
+    }
+
+    ngOnInit() {
+        this.optionsWithAll.set([this.allOption, ...this.options()]);
+        this.selectedValues.set([]);
+    }
+
+    private handleMultipleSelection(selectedValues: Option[], clickedItem: Option): Option[] {
+        if (this.isAllOptionSelected(clickedItem)) {
+            this.isAllOptionsChecked = !this.isAllOptionsChecked;
+            return this.isAllOptionsChecked ? this.optionsWithAll() : [];
+        }
+
+        this.isAllOptionsChecked = false;
+        return selectedValues.filter((option) => option.value !== this.allOption.value);
+    }
+
+    private isAllOptionSelected(option: Option): boolean {
+        return option.value === this.allOption.value;
+    }
 }
--- src/app/shared/components/layout/filter-panel/filter-panel.component.ts
+++ src/app/shared/components/layout/filter-panel/filter-panel.component.ts
@@ -1,13 +1,21 @@
 import { Component } from '@angular/core';
-import { CardComponent } from "../card/card.component";
-import { SelectDropdownComponent } from "../../forms/select-dropdown/select-dropdown.component";
+import { CardComponent } from '../card/card.component';
+import { SelectDropdownComponent } from '../../forms/select-dropdown/select-dropdown.component';
 
 @Component({
-  selector: 'app-filter-panel',
-  imports: [CardComponent, SelectDropdownComponent],
-  templateUrl: './filter-panel.component.html',
-  styleUrl: './filter-panel.component.scss'
+    selector: 'app-filter-panel',
+    imports: [CardComponent, SelectDropdownComponent],
+    templateUrl: './filter-panel.component.html',
+    styleUrl: './filter-panel.component.scss'
 })
 export class FilterPanelComponent {
+    options = [
+        { value: '1', label: 'Option 1' },
+        { value: '2', label: 'Option 2' },
+        { value: '3', label: 'Option 3' }
+    ];
 
+    onSelectionChange(selected): void {
+        console.log(selected);
+    }
 }
