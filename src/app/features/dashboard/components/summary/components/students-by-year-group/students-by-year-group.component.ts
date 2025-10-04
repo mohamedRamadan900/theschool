6f7bfaca0e59bb24e13fbf671879e82d28867a0a
Mahmoud Meky <mahmoudmeky2017@gmail.com>
2025-10-04 23:06:46 +0300

    add date picker

--- src/app/features/dashboard/components/summary/components/students-by-year-group/students-by-year-group.component.ts
+++ src/app/features/dashboard/components/summary/components/students-by-year-group/students-by-year-group.component.ts
@@ -1,25 +1,37 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, effect, inject, OnInit, signal } from '@angular/core';
 import { IStackedBarChartFilter, StackedBarChartConfig } from '../../../../../../shared/components/charts/stacked-bar-chart/stacked-bar-chart-model';
 import { ChartColorsArray } from '../../../../../../shared/components/charts/chart-colors';
 import { StackedBarChart } from '../../../../../../shared/components/charts/stacked-bar-chart/stacked-bar-chart.component';
 import { SchoolDataAPIService } from '../../../../services/school-data.service';
 import { YearGroupStats } from '../../../../models/dashboard.interface';
 import { SummaryService } from '../../services/summary.service';
+import { DatePickerModule } from 'primeng/datepicker';
+import { FormsModule } from '@angular/forms';
 
 @Component({
     selector: 'app-dashboard-students-by-year-group',
-    imports: [StackedBarChart],
+    imports: [StackedBarChart, DatePickerModule, FormsModule],
     templateUrl: './students-by-year-group.component.html',
     styleUrl: './students-by-year-group.component.scss'
 })
-export class StudentsByYearGroupComponent implements OnInit {
+export class StudentsByYearGroupComponent {
     private schoolDataService = inject(SchoolDataAPIService);
     private summaryService = inject(SummaryService);
 
+    // date: Date | null = new Date();
+    date = signal(new Date());
     studentsByYearGroupAndSexBarChart: StackedBarChartConfig;
 
-    ngOnInit(): void {
+    constructor() {
+        effect(() => {
+            this.getStudentsYearGroupData();
+        });
+    }
+
+    private getStudentsYearGroupData(): void {
+        const selectedDate = this.date(); //! Should be passed to Endpoint
         this.schoolDataService.getStudentsYearGroup().subscribe((data) => {
+            console.log('Get Data');
             this.studentsByYearGroupAndSexBarChart = this.prepareChartConfig(data);
         });
     }
@@ -54,7 +66,7 @@ export class StudentsByYearGroupComponent implements OnInit {
             direction: 'horizontal',
             showTotals: false,
             hideDataLabels: false,
-            showLegend: true
+            showLegend: true,
             // readOnly: true
         };
     }
